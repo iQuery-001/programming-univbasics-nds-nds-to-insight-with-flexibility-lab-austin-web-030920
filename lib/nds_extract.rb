4a0663b76a61391a6394f2599f9f18323e946b00
Dakota Willingham <kota@vaga.tk>
2020-02-26 02:17:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -96,8 +96,10 @@ def movies_with_directors_set(source)
   #
   # RETURN:
   #
-  # Array of Arrays containing all of a director's movies. Each movie will need
+  # Array of Hashes containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  movie_counter = 0
+  director_coll = []
 end
 
 # ----------------    End of Your Code Region --------------------
